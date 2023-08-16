dbb595c407a32982f5848613f1e308f328e992d7
SingJn <33142878+SingJn@users.noreply.github.com>
2023-08-16 20:43:40 +0900

    Initial Commit

---
+++ ColorCalculator/ColorCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCalculatorApp.swift
+//  ColorCalculator
+//
+//  Created by 박상준 on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct ColorCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
