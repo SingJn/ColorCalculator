8b6863dee582a321a40c68ecc64148f886c07dc1
SingJn <33142878+SingJn@users.noreply.github.com>
2023-08-23 14:27:59 +0900

    [Invaild] Refactoring code for clarity

---
+++ ColorCalculator/Functions.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  ColorCalculator
+//
+//  Created by 박상준 on 2023/08/22.
+//
+enum SelectedOperator: String {
+    case plus = "+"
+    case minus = "–"
+    case muliply = "×"
+    case divide = "÷"
+    case equal = "="
+}
